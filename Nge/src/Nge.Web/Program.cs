bf0a1c62dfcb80bc6abc6c19a66dee18b7cdd1a5
Roman Buldygin <froooks@gmail.com>
2016-06-20 15:23:07 +0700

    Изменение что бы вызвать билд

--- Nge/src/Nge.Web/Program.cs
+++ Nge/src/Nge.Web/Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-
-namespace Nge.Web
+﻿namespace Nge.Web
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+
     public class Program
     {
         public static void Main(string[] args)
