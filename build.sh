ebe8331bc5844b5cbb62da08f1fa82381da32e96
Roman Buldygin <froooks@gmail.com>
2016-06-20 21:13:14 +0700

    Изменение скрипта сборки

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dotnet restore
+dotnet restore --source="https://www.myget.org/F/npgsql-unstable/api/v3/index.json;https://api.nuget.org/v3/index.json"
 for path in Nge/src/*/project.json; do
     dirname="$(dirname "${path}")"
     dotnet build ${dirname} -c Release
