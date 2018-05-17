7eddd60c54a430f4ba31214a71a7b5ce40bd36e5
Roman <buldo@mail.ru>
2018-05-17 23:15:13 +0300

    Немного VM для Play

---
+++ src/Nge.Web/Models/PlayViewModels/CodeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nge.Web.Models.PlayViewModels
+{
+    public class CodeViewModel
+    {
+        public bool IsEntered { get; set; }
+        public string CodeType { get; set; }
+    }
+}
---
+++ src/Nge.Web/Models/PlayViewModels/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nge.Web.Models.PlayViewModels
+{
+    public class IndexViewModel
+    {
+        public List<CodeViewModel> Codes;
+    }
+}
