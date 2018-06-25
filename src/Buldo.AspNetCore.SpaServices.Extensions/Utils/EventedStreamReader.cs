7d7da94e459baf21ff2b5bcb03e7a7fbb439f048
Roman <buldo@mail.ru>
2018-06-25 23:16:40 +0300

    Вернул копирайты

--- src/Buldo.AspNetCore.SpaServices.Extensions/Utils/EventedStreamReader.cs
+++ src/Buldo.AspNetCore.SpaServices.Extensions/Utils/EventedStreamReader.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Buldo.AspNetCore.SpaServices.Extensions.Utils
